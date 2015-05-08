9744b4732059687a3ea211a886ddc66d64006167
s-zhouj <s-zhouj@54L1809-26.it.bsd405.org>
2015-05-08 08:00:43 -0700

    Frame setup, init commit~

---
+++ SpaceBattle/src/spacebattle/Program.java
@@ -0,0 +1,29 @@
+package spacebattle;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author s-zhouj
+ */
+public class Program {
+
+    public static final int worldWidth = 1024;
+    public static final int worldHeight = 768;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public Point direction(Point p1, Point p2) {
+
+    }
+
+    public double distance(Point p1, Point p2) {
+
+    }
+
+}
