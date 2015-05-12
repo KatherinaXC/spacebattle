55b73fa294be338bbbedfaa53677e9e3a49a77b9
s-zhouj <s-zhouj@54L1809-26.it.bsd405.org>
2015-05-12 08:03:47 -0700

    Server setup

---
+++ SpaceBattleClient/src/CenterShip.java
@@ -0,0 +1,27 @@
+
+import java.awt.Color;
+import ihs.apcs.spacebattle.*;
+import ihs.apcs.spacebattle.commands.*;
+
+/**
+ *
+ * @author s-zhouj
+ */
+public class CenterShip extends BasicSpaceship {
+
+    @Override
+    public RegistrationData registerShip(int i, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ShipCommand getNextCommand(BasicEnvironment be) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void shipDestroyed() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
