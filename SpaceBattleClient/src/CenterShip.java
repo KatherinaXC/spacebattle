5bfc4273409d5c5072f6e13dec46df5a5cea833f
Winterstorm <Winterstorm@M400>
2015-05-15 23:41:26 -0700

    ... Should... BASICALLY... work? Probably should add some elaborations and failsafes too.

--- SpaceBattleClient/src/CenterShip.java
+++ SpaceBattleClient/src/CenterShip.java
@@ -13,36 +13,72 @@ public class CenterShip extends BasicSpaceship {
     private static int worldHeight;
 
     private ShipState state = ShipState.START;
-    private ExactPoint[] waypoints;
+    private Point[] waypoints;
+    private int current = 0;
 
     @Override
     public RegistrationData registerShip(int numImages, int worldWidth, int worldHeight) {
         this.worldWidth = worldWidth;
         this.worldHeight = worldHeight;
+        //Initialize waypoints, specific to a CenterShip
+        this.waypoints = new Point[1];
+        this.waypoints[0] = new Point(this.worldWidth / 2, this.worldHeight / 2);
+        //End init
         return new RegistrationData("arachnidsGrip", new Color(00, 41, 82), numImages);
     }
 
     @Override
     public ShipCommand getNextCommand(BasicEnvironment be) {
-        switch (this.state) {
-            case START:
-                //Initialize waypoints
-                this.waypoints = new ExactPoint[1];
-                this.waypoints[0] = new ExactPoint(this.worldWidth / 2, this.worldHeight / 2);
-                this.state = ShipState.TURN;
-                break;
-            case TURN:
-                break;
-            case THRUST:
-                break;
-            case COAST:
-                break;
-            case BRAKE:
-                break;
-            case STOP:
-                break;
+        ObjectStatus shipStatus = be.getShipStatus();
+        Point optimalVect = this.direction(shipStatus.getPosition(), waypoints[current]);
+        double optimalDirection = -1 * (Math.toDegrees(Math.atan(optimalVect.getY() / optimalVect.getX())));
+        double distance = this.distance(shipStatus.getPosition(), waypoints[current]);
+        double currentDirection = shipStatus.getOrientation();
+        double currentSpeed = shipStatus.getSpeed();
+        while (true) {
+            switch (this.state) {
+                case TURN:
+                    optimalDirection = (optimalDirection + 360) % 360;
+                    System.out.println("TEST MY ANGLE TO DESTINATION IS " + optimalDirection);
+                    System.out.println("TEST MY ANGLE IS " + currentDirection);
+                    if (Math.abs(currentDirection - optimalDirection) < 1) {
+                        this.state = ShipState.THRUST;
+                    } else {
+                        return new RotateCommand(optimalDirection - currentDirection);
+                    }
+                    break;
+                case THRUST:
+                    if (currentSpeed > distance / 2) {
+                        this.state = ShipState.BRAKE;
+                    } else if (currentSpeed < shipStatus.getMaxSpeed()) {
+                        return new ThrustCommand('B', 0.1, 0.1);
+                    } else {
+                        this.state = ShipState.COAST;
+                    }
+                    break;
+                case COAST:
+                    if (distance > currentSpeed / 2) {
+                        return new IdleCommand(0.1);
+                    } else {
+                        this.state = ShipState.BRAKE;
+                    }
+                    break;
+                case BRAKE:
+                    return new BrakeCommand(0.1);
+                case STOP:
+                    if (waypoints.length >= current) {
+                        current++;
+                        this.state = ShipState.START;
+                    } else {
+                        return new IdleCommand(0.1);
+                    }
+                    break;
+                default:
+                    // catchall
+                    this.state = ShipState.TURN;
+                    break;
+            }
         }
-        return new IdleCommand(0.1);
     }
 
     @Override
@@ -56,12 +92,12 @@ public class CenterShip extends BasicSpaceship {
      * @param p2 the ending point
      * @return vector representing shortest distance
      */
-    public java.awt.Point direction(java.awt.Point p1, java.awt.Point p2) {
+    public Point direction(Point p1, Point p2) {
         //Set up variables
-        int dx = p2.x - p1.x;
-        int dy = p2.y - p1.y;
-        int centerx = this.worldWidth / 2;
-        int centery = this.worldHeight / 2;
+        double dx = p2.getX() - p1.getX();
+        double dy = p2.getY() - p1.getY();
+        double centerx = this.worldWidth / 2;
+        double centery = this.worldHeight / 2;
         //Determine if I need to wrap
         boolean wrapwidth = dx > centerx;
         boolean wrapheight = dy > centery;
@@ -73,7 +109,7 @@ public class CenterShip extends BasicSpaceship {
             dy -= this.worldHeight;
         }
         //Return a new Point
-        return new java.awt.Point(dx, dy);
+        return new Point(dx, dy);
     }
 
     /**
@@ -83,9 +119,9 @@ public class CenterShip extends BasicSpaceship {
      * @param p2 the ending point
      * @return the shortest distance between two points
      */
-    public double distance(java.awt.Point p1, java.awt.Point p2) {
-        java.awt.Point vector = direction(p1, p2);
-        return Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+    public double distance(Point p1, Point p2) {
+        Point vector = direction(p1, p2);
+        return Math.sqrt(vector.getX() * vector.getX() + vector.getY() * vector.getY());
     }
 
     /**
@@ -96,12 +132,12 @@ public class CenterShip extends BasicSpaceship {
      * @param distToGo the distance to travel forward
      * @return
      */
-    public static ExactPoint targetDest(java.awt.Point current, double angle, double distToGo) {
+    public static Point targetDest(Point current, double angle, double distToGo) {
         double finalX = current.getX() + distToGo * (Math.cos(Math.toRadians(angle)));
         double finalY = current.getY() - distToGo * (Math.sin(Math.toRadians(angle)));
         finalX = CenterShip.wrap(finalX, CenterShip.worldWidth);
         finalY = CenterShip.wrap(finalY, CenterShip.worldHeight);
-        return new ExactPoint(finalX, finalY);
+        return new Point(finalX, finalY);
     }
 
     /**
@@ -131,35 +167,35 @@ public class CenterShip extends BasicSpaceship {
         CenterShip test = new CenterShip();
 
         System.out.println("Testing Vector Direction");
-        java.awt.Point p1 = new java.awt.Point(0, 0);
-        java.awt.Point p2 = new java.awt.Point(5, 0);
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(5, 0);
         System.out.println("Test\t" + p1 + p2);
         System.out.println("Vector\t" + test.direction(p1, p2));
         System.out.println("Distance\t" + test.distance(p1, p2));
-        p1 = new java.awt.Point(0, 0);
-        p2 = new java.awt.Point(1000, 0);
+        p1 = new Point(0, 0);
+        p2 = new Point(1000, 0);
         System.out.println("Test\t" + p1 + p2);
         System.out.println("Vector\t" + test.direction(p1, p2));
         System.out.println("Distance\t" + test.distance(p1, p2));
-        p1 = new java.awt.Point(0, 0);
-        p2 = new java.awt.Point(0, 5);
+        p1 = new Point(0, 0);
+        p2 = new Point(0, 5);
         System.out.println("Test\t" + p1 + p2);
         System.out.println("Vector\t" + test.direction(p1, p2));
         System.out.println("Distance\t" + test.distance(p1, p2));
-        p1 = new java.awt.Point(0, 0);
-        p2 = new java.awt.Point(0, 700);
+        p1 = new Point(0, 0);
+        p2 = new Point(0, 700);
         System.out.println("Test\t" + p1 + p2);
         System.out.println("Vector\t" + test.direction(p1, p2));
         System.out.println("Distance\t" + test.distance(p1, p2));
-        p1 = new java.awt.Point(0, 0);
-        p2 = new java.awt.Point(1000, 700);
+        p1 = new Point(0, 0);
+        p2 = new Point(1000, 700);
         System.out.println("Test\t" + p1 + p2);
         System.out.println("Vector\t" + test.direction(p1, p2));
         System.out.println("Distance\t" + test.distance(p1, p2));
 
         System.out.println();
         System.out.println("Testing Target Point");
-        p1 = new java.awt.Point(5, 5);
+        p1 = new Point(5, 5);
         System.out.println("Origin:\t" + p1);
         System.out.println("Right 5:\t" + CenterShip.targetDest(p1, 0, 5));
         System.out.println("Up 5:\t" + CenterShip.targetDest(p1, 90, 5));
