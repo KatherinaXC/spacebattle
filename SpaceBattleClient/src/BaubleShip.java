dc00aa6ad7084879fce4d73234a4eac93b92e70e
s-zhouj <s-zhouj@54L1809-26.it.bsd405.org>
2015-05-28 09:39:24 -0700

    init baubleship

---
+++ SpaceBattleClient/src/BaubleShip.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author s-zhouj
+ */
+public class BaubleShip extends AsteroidShip {
+
+    /**
+     * Constructor for a BaubleShip, setting up the parameters worldWidth and
+     * worldHeight.
+     *
+     * @param worldWidth the width of the world
+     * @param worldHeight the height of the world
+     */
+    public BaubleShip(int worldWidth, int worldHeight) {
+        super(worldWidth, worldHeight);
+    }
+}
